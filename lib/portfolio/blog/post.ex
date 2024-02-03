6801a46d6b651f917a6bc43e074590ec7df265df
Risson <aime.risson.1@gmail.com>
2024-02-03 17:31:01 +0100

    blog post json ld

--- lib/portfolio/blog/post.ex
+++ lib/portfolio/blog/post.ex
@@ -1,11 +1,31 @@
 defmodule Portfolio.Blog.Post do
   @enforce_keys [:id, :author, :title, :body, :description, :tags, :date]
-  defstruct [:id, :author, :title, :body, :description, :tags, :date]
+  defstruct [:id, :author, :title, :body, :description, :tags, :date, :json_ld]
 
   def build(filename, attrs, body) do
     [year, month_day_id] = filename |> Path.rootname() |> Path.split() |> Enum.take(-2)
     [month, day, id] = String.split(month_day_id, "-", parts: 3)
     date = Date.from_iso8601!("#{year}-#{month}-#{day}")
-    struct!(__MODULE__, [id: id, date: date, body: body] ++ Map.to_list(attrs))
+    struct!(__MODULE__, [id: id, date: date, body: body, json_ld: post_json_ld(attrs, date)] ++ Map.to_list(attrs))
+  end
+
+
+  def post_json_ld(attrs, date) do
+
+    """
+    {
+          "@context": "https://schema.org",
+          "@type": "NewsArticle",
+          "headline": "#{attrs.title}",
+          "image": [],
+          "datePublished": "#{date}",
+          "dateModified": "2015-02-05T09:20:00+08:00",
+          "author": [{
+              "@type": "Person",
+              "name": "Aimé Risson",
+              "url": "https://herisson.dev"
+            }]
+        }
+    """
   end
 end
